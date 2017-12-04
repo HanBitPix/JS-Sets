2a867e2f12963507c481916283bdf057e764d4c4
Linda Foo <linda.foo.dev@gmail.com>
2017-12-04 15:09:42 -0800

    Completed final parts of JS set 2

--- Set-2.js
+++ Set-2.js
@@ -120,3 +120,97 @@ function decode(word){
 }
 decode("Chicken Soup");
 */
+
+/* Write a function that takes the name of a month as an argument and use a switch statement to determine how many days are there in that month. For example if the parameter to the function is January, the function should display "January has 31 days".
+
+*/
+
+function daysInMonth(month) {
+  let monthNew = month.toLowerCase();
+  switch(monthNew)
+  {
+    case "january":
+    console.log("January has 31 days");
+    break;
+
+    case "february":
+    console.log("February has 28 days");
+    break;
+
+    case "march":
+    console.log("March has 31 days");
+    break;
+
+    case "april":
+    console.log("April has 30 days");
+    break;
+
+    case "may":
+    console.log("May has 31 days");
+    break;
+
+    case "june":
+    console.log("June has 30 days");
+    break;
+
+    case "july":
+    console.log("July has 31 days");
+    break;
+
+    case "august":
+    console.log("August has 31 days");
+    break;
+
+    case "september":
+    console.log("September has 30 days");
+    break;
+
+    case "october":
+    console.log("October has 31 days");
+    break;
+
+    case "november":
+    console.log("November has 30 days");
+    break;
+
+    case "december":
+    console.log("Dovember has 31 days");
+    break;
+
+    default:
+    console.log("ERROR");
+    break;
+  }
+
+}
+
+daysInMonth("January");
+
+function RockPaperScissors(userChoice){
+  var computer = 1 + Math.floor((Math.random()*3));
+  if (computer === 1 && userChoice === 2) {
+    console.log("Paper beats rock, because a piece of paper can cover a rock");
+  }
+  else if (computer === 1 && userChoice === 3) {
+    console.log("Rock beats scissors - a rock can break a pair of scissors");
+  }
+  else if (computer === 2 && userChoice === 1) {
+    console.log("Paper beats rock, because a piece of paper can cover a rock");
+  }
+  else if (computer === 2 && userChoice === 3) {
+    console.log("Scissor beats paper, beacuse scissors can cut paper");
+  }
+  else if (computer === 3 && userChoice === 1) {
+    console.log("Rock beats scissors - a rock can break a pair of scissors");
+  }
+  else if (computer === 3 && userChoice === 2) {
+    console.log("Scissor beats paper, beacuse scissors can cut paper");
+  }
+  else if (computer === userChoice) {
+    console.log("It's a tie!")
+  }
+}
+
+RockPaperScissors(2);
+RockPaperScissors(3);
+RockPaperScissors(1);
